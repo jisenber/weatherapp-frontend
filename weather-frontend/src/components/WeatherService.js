d4dab710116e55896b3ed7dbd7023744fef2a6e3
jisenber <isenberg.jacob@yahoo.com>
2017-09-28 00:29:46 -0700

    charts appear for all searches. need to improve quality of charts

--- weather-frontend/src/components/WeatherService.js
+++ weather-frontend/src/components/WeatherService.js
@@ -41,7 +41,6 @@ class WeatherService {
       url: `http://localhost:4200/timeforecast?lat=${lat}&lng=${lng}&time=${time}`
     })
     .then(response => {
-      console.log(response);
       var historicForecast = response.data.daily.data;
       cb(historicForecast);
     });
